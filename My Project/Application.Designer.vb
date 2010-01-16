71823f5b242e78cbbc0502bbd1ec866d4f61c74d
Daniel Danopia <me@danopia.net>
2010-01-15 19:54:57 -0500

    Forgot these

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.CombatCorps.Form1
+            Me.MainForm = Global.CombatCorps.frmLogin
         End Sub
     End Class
 End Namespace
